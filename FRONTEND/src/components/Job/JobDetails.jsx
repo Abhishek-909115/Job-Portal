7d6ac84e1ebd880aeb6d93c19f1e35aaa31c5a45
Abhishek Kumar <abhishek909115@gmail.com>
2024-08-18 02:44:03 +0530

    Update JobDetails.jsx

--- FRONTEND/src/components/Job/JobDetails.jsx
+++ FRONTEND/src/components/Job/JobDetails.jsx
@@ -12,7 +12,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      .get(`https://job-seeker-aff4.onrender.com/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
