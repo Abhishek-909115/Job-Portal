1946c0080c817170b643a3b5201b8f1a956f7ba2
Abhishek Kumar <abhishek909115@gmail.com>
2024-08-18 02:34:50 +0530

    Update MyApplication.jsx

--- FRONTEND/src/components/Application/MyApplication.jsx
+++ FRONTEND/src/components/Application/MyApplication.jsx
@@ -18,7 +18,7 @@ const MyApplications = () => {
     try {
       if (user && user.role === "Employer") {
         axios
-          .get("http://localhost:4000/api/v1/application/employer/getall", {
+          .get("https://job-seeker-aff4.onrender.com/api/v1/application/employer/getall", {
             withCredentials: true,
           })
           .then((res) => {
