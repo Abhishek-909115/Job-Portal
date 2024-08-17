14220d8911e280e50205320384c8b634c38f5a20
Abhishek Kumar <abhishek909115@gmail.com>
2024-08-18 02:41:29 +0530

    Update Login.jsx

--- FRONTEND/src/components/Auth/Login.jsx
+++ FRONTEND/src/components/Auth/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://job-seeker-aff4.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
