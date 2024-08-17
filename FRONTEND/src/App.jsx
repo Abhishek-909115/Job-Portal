bd47c21cda969553047990ff688177755a976d65
Abhishek Kumar <abhishek909115@gmail.com>
2024-08-18 02:38:46 +0530

    Update App.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          "https://job-seeker-aff4.onrender.com/api/v1/user/getuser",
           {
             withCredentials: true,
           }
