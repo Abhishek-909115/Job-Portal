8ddd1d38ad2ccacfad49734f835bea193c4a0bd1
Abhishek Kumar <abhishek909115@gmail.com>
2024-08-18 02:48:02 +0530

    Update Navbar.jsx

--- FRONTEND/src/components/Layout/Navbar.jsx
+++ FRONTEND/src/components/Layout/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://job-seeker-aff4.onrender.com/api/v1/user/logout",
         {
           withCredentials: true,
         }
