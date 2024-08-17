05ef3ba4c09ec8d091846d9257990620a3b3ca87
Abhishek Kumar <abhishek909115@gmail.com>
2024-08-18 02:42:19 +0530

    Update Register.jsx

--- FRONTEND/src/components/Auth/Register.jsx
+++ FRONTEND/src/components/Auth/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/register",
+        "https://job-seeker-aff4.onrender.com/api/v1/user/register",
         { name, phone, email, role, password },
         {
           headers: {
