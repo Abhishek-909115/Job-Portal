43cdfce34ce77f7fc998f884fe1b64b15cb533d9
Abhishek Kumar <abhishek909115@gmail.com>
2024-08-18 17:09:10 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -25,11 +25,11 @@ dotenv.config({path:"./config/config.env"});
 
 //   next();
 // });
-res.cookie('token', token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production', // Ensure cookies are only sent over HTTPS in production
-  sameSite: 'None', // Allow cross-origin requests
-});
+// res.cookie('token', token, {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === 'production', // Ensure cookies are only sent over HTTPS in production
+//   sameSite: 'None', // Allow cross-origin requests
+// });
 
 
 app.use(
