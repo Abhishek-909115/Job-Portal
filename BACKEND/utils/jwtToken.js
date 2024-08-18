719d47a4d85abf15b91d47835b0f8ee837201c8f
Abhishek Kumar <abhishek909115@gmail.com>
2024-08-18 20:08:56 +0530

    Update jwtToken.js

--- BACKEND/utils/jwtToken.js
+++ BACKEND/utils/jwtToken.js
@@ -4,7 +4,10 @@ export const sendToken = (user, statusCode, res, message) => {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true, // Set httpOnly to true
+         httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+    sameSite: 'None',
+      
     };
   
     res.status(statusCode).cookie("token", token, options).json({
