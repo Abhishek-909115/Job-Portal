f9fb86cbf1aebab8be1a175abf5b463f40d0a000
Abhishek Kumar <abhishek909115@gmail.com>
2024-08-18 19:35:34 +0530

    Update userController.js

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -58,11 +58,9 @@ export const logout = catchAsyncError(async (req, res, next) => {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production', // Ensure cookies are only sent over HTTPS in production
   sameSite: 'None', // Allows cross-origin requests
+         expires: new Date(Date.now())
 });
-
-      sameSite:"none",
-      expires: new Date(Date.now()),
-    })
+ })
     .json({
       success: true,
       message: "Logged Out Successfully.",
